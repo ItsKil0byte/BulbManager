dab45cab6206dc737579109691f8731cd657ac56
Alexander Lebedev <itskilobyte@gmail.com>
2026-01-15 16:49:32 +0500

    add new methods for API service

--- app/src/main/java/com/example/bulbproject/data/BulbService.kt
+++ app/src/main/java/com/example/bulbproject/data/BulbService.kt
@@ -3,15 +3,22 @@ package com.example.bulbproject.data
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface BulbService {
 
-    @POST("/state/on")
-    suspend fun powerOn(): Response<Boolean>
-
-    @POST("/state/off")
-    suspend fun powerOff(): Response<Boolean>
-
     @GET("/state/")
     suspend fun checkPower(): Response<Boolean>
+
+    @POST("/state/on")
+    suspend fun powerOn(): Response<Unit>
+
+    @POST("/state/off")
+    suspend fun powerOff(): Response<Unit>
+
+    @POST("/state/brightness")
+    suspend fun setBrightness(@Query("value") value: Int) : Response<Unit>
+
+    @POST("state/color")
+    suspend fun setColor(@Query("color") color: String): Response<Unit>
 }
