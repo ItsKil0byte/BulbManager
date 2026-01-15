f3174e658bc764e632abb21138ccc9d641c62eea
Alexander Lebedev <itskilobyte@gmail.com>
2026-01-15 18:01:18 +0500

    add seek bar for brightness

--- app/src/main/java/com/example/bulbproject/presentier/MainFragment.kt
+++ app/src/main/java/com/example/bulbproject/presentier/MainFragment.kt
@@ -3,7 +3,7 @@ package com.example.bulbproject.presentier
 import android.content.Context
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.bulbproject.R
@@ -13,7 +13,7 @@ import com.example.bulbproject.di.viewModel.ViewModelFactory
 import dev.androidbroadcast.vbpd.viewBinding
 import javax.inject.Inject
 
-class MainFragment: Fragment(R.layout.main_fragment) {
+class MainFragment : Fragment(R.layout.main_fragment) {
 
     val binding: MainFragmentBinding by viewBinding(MainFragmentBinding::bind)
 
@@ -23,9 +23,32 @@ class MainFragment: Fragment(R.layout.main_fragment) {
     private val viewModel: MainViewModel by viewModels { viewModelFactory }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         binding.btn.setOnClickListener {
             viewModel.toggle()
         }
+
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar?,
+                progress: Int,
+                fromUser: Boolean
+            ) {
+                if (fromUser) {
+                    viewModel.setBrightness(progress)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Skip
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Skip
+            }
+
+        })
     }
 
     override fun onAttach(context: Context) {
