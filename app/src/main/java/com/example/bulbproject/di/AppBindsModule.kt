7cdcab2259bbf09a4070da20e8633bb106467cae
Alexander Lebedev <itskilobyte@gmail.com>
2026-01-15 19:11:20 +0500

    add a missing @Binds

--- app/src/main/java/com/example/bulbproject/di/AppBindsModule.kt
+++ app/src/main/java/com/example/bulbproject/di/AppBindsModule.kt
@@ -2,6 +2,8 @@ package com.example.bulbproject.di
 
 import com.example.bulbproject.data.BulbRepository
 import com.example.bulbproject.data.BulbRepositoryImpl
+import com.example.bulbproject.domain.GetInfoUseCase
+import com.example.bulbproject.domain.GetInfoUseCaseImpl
 import com.example.bulbproject.domain.SetBrightnessUseCase
 import com.example.bulbproject.domain.SetBrightnessUseCaseImpl
 import com.example.bulbproject.domain.SetColorUseCase
@@ -16,6 +18,9 @@ interface AppBindsModule {
 
     @Binds
     fun bindBulbRepository(impl: BulbRepositoryImpl): BulbRepository
+
+    @Binds
+    fun bindGetInfoUseCase(impl: GetInfoUseCaseImpl): GetInfoUseCase
 
     @Binds
     fun bindToggleBulbUseCase(impl: ToggleBulbUseCaseImpl): ToggleBulbUseCase
